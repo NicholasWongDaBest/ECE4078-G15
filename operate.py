370b1e95a28e499789907193b5fa0e4e37fbe962
BrandonGan <nbrandongyy@gmail.com>
2026-08-04 09:06:05 +0800

    Implement keyboard wheel speed controls
    Replaces the TODO stubs in `operate.py` with concrete manual drive behavior: arrow keys now set forward/reverse/turn wheel speeds, and key release/space stops the robot. It also sends the current wheel speed to `botconnect.move_manual(...)` during event handling so keyboard inputs immediately drive the robot.

--- operate.py
+++ operate.py
@@ -206,19 +206,19 @@ class Operate:
         for event in pygame.event.get():
             # drive forward
             if event.type == pygame.KEYDOWN and event.key == pygame.K_UP:
-                pass # TODO
+                self.command['wheel_speed'] = [0.6, 0.6]
             # drive backward
             elif event.type == pygame.KEYDOWN and event.key == pygame.K_DOWN:
-                pass # TODO
+                self.command['wheel_speed'] = [-0.6, -0.6]
             # turn left
             elif event.type == pygame.KEYDOWN and event.key == pygame.K_LEFT:
-                pass # TODO
+                self.command['wheel_speed'] = [-0.6, 0.6]
             # drive right
             elif event.type == pygame.KEYDOWN and event.key == pygame.K_RIGHT:
-                pass # TODO
+                self.command['wheel_speed'] = [0.6, -0.6]
             # stop (set speed to zero)
             elif event.type == pygame.KEYUP or (event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE):
-                pass # TODO
+                self.command['wheel_speed'] = [0, 0]
             # run SLAM
             elif event.type == pygame.KEYDOWN and event.key == pygame.K_RETURN:
                 n_observed_markers = len(self.ekf.taglist)
@@ -257,13 +257,16 @@ class Operate:
             elif event.type == pygame.KEYDOWN and event.key == pygame.K_n:
                 self.command['save_obj_detector'] = True
             # capture and save raw image
-            elif event.type == pygame.KEYDOWN and event.key == pygame.K_i:
+            elif event.type == pygame.KEYDOWN and event.key  == pygame.K_i:
                 self.command['save_image'] = True
             # quit
             elif event.type == pygame.QUIT:
                 self.quit = True
             elif event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
                 self.quit = True
+
+            self.botconnect.move_manual(self.command['wheel_speed'])
+
         if self.quit:
             pygame.quit()
             sys.exit()
